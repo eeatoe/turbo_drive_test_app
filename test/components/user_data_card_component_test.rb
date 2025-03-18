0b5a3141f80e7d9c9e73fb91ad6c9508a83d29b7
Sergey <eeatoe@gmail.com>
2025-03-18 05:41:37 -0400

    first commit

---
+++ test/components/user_data_card_component_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class UserDataCardComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(UserDataCardComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
